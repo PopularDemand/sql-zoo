a61fab5985b36c4fbf7ce150c9fc929dc837ae83
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-28 14:22:15 -0800

    problem 8

--- joins.sql
+++ joins.sql
@@ -1,6 +1,6 @@
 GAME
 =====
-game id   mdate     stadium                    team1  team2
+ id   mdate     stadium                    team1  team2
 1001  8 June 2012   National Stadium, Warsaw    POL   GRE
 1002  8 June 2012   Stadion Miejski (Wroclaw)   RUS   CZE
 1003 12 June 2012   Stadion Miejski (Wroclaw)   GRE   CZE
@@ -30,7 +30,63 @@ GRE   Greece           Fernando Santos
 
 1
 
-SELECT matchid, player FROM goal 
+SELECT matchid, player FROM goal
   WHERE teamid = 'Ger'
 
+----------------------------
+
+----------------------------
+
+2
+
+SELECT DISTINCT id, stadium,team1,team2
+  FROM goal JOIN game ON matchid = id
+WHERE id = 1012
+
+
+--------------------------------------------------------
+
+3
+
+SELECT player, teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid = "GER"
+
+
+------------------------------------------------------------------------------------
+
+4
+
+SELECT team1, team2, player
+  FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'
+
+
+-------------------------------------
+
+5
+
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON teamid = id
+ WHERE gtime<=10
+
+----------------------------
+-------------------------------------
+
+6
+SELECT mdate, teamname
+FROM game JOIN eteam ON (team1 = eteam.id)
+WHERE eteam.coach = 'Fernando Santos'
+----------------------------
+
+7
+SELECT player
+FROM game JOIN goal ON game.id = goal.matchid
+WHERE stadium = 'National Stadium, Warsaw'
+----------------------------
+
+8
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id
+    WHERE (team1='GER' OR team2='GER') AND (goal.teamid != "GER")
 ----------------------------
